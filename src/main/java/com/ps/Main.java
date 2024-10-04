c6d0d11a3499156e667d46ca763e7a0ded49e6a4
linotmike <128604219+linotmike@users.noreply.github.com>
2024-10-03 18:35:35 -0700

    Made an array to hold upto 20 cars preloaded 6 into the array

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -2,7 +2,15 @@ package com.ps;
 
 public class Main {
 public static void main(String [] args){
+Vehicle [] vehicle = new Vehicle[20];
 
+int count = 6;
+vehicle[0] = new Vehicle(101121, "Ford Explorer","Red",45000,13500);
+vehicle[1] = new Vehicle(101122,"Toyota Camry","Blue",60000,11000);
+vehicle[2] = new Vehicle(101123,"Chevrolet Malibu","Black",50000,9700);
+vehicle[3] = new Vehicle(101124,"Honda Civic","White",70000,7500);
+vehicle[4] = new Vehicle(101125,"Subaru Outback","Green",55000,14500);
+vehicle[5] = new Vehicle(101126,"Jeep Wrangler","Yellow",30000,16000);
 
 };
 }
