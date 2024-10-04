572553c70eabf4fec12e53f744ff2884e08c3850
linotmike <128604219+linotmike@users.noreply.github.com>
2024-10-03 18:11:14 -0700

    added vehicle class

---
+++ src/main/java/com/ps/Vehicle.java
@@ -0,0 +1,18 @@
+package com.ps;
+//Create a class named Vehicle. Add the following data members to it:
+//vehicleId - a long (ex: 101121)
+//makeModel - a string (ex: Ford Explorer)
+//color - a string (ex: Red)
+//odometerReading - an int (ex: 32775)
+//price - a float (ex: 12250.00) (no Lamborghinis here!)
+//Add a constructor and get/set methods for each property of the car.
+
+public class Vehicle {
+    private long vehicleId  ;
+    private String makeModel;
+    private String color;
+    private int odometerReading;
+    private float price;
+
+
+}
