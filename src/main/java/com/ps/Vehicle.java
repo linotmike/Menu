94ee5179c49e77eda3f3421bad217ea924b0cf07
linotmike <128604219+linotmike@users.noreply.github.com>
2024-10-03 18:15:41 -0700

    Added getters and setters for vehicle class

--- src/main/java/com/ps/Vehicle.java
+++ src/main/java/com/ps/Vehicle.java
@@ -15,4 +15,42 @@ public class Vehicle {
     private float price;
 
 
+    public long getVehicleId(){
+        return this.vehicleId;
+    }
+    public void setVehicleId(long vehicleId){
+        this.vehicleId = vehicleId;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
